22e905d72b9a49612c00449b237abeef32c95e5d
Kyle Boehlen <kyleboehlen@gmail.com>
2019-11-15 21:57:00 -0700

    Base header template and styles

--- resources/views/layouts/semantic.blade.php
+++ resources/views/layouts/semantic.blade.php
@@ -3,11 +3,20 @@
 @section('template')
     {{-- header based on auth status --}}
     <header>
+        {{-- Logo --}}
         <img id="header-logo" src="{{ route('assets.logo') }}" />
+
+        {{-- Title and filters --}}
         @if(isset($show) && in_array($show, 'header_filters'))
             {{-- placeholder for filters that will replace the title --}}
         @else
             <h1 class="title">{{ config('app.name', 'P&N') }}</h1>
+        @endif
+
+        @if(Auth::check())
+            {{-- Browse link --}}
+        @else
+            <a class="auth-link" href="{{-- Sign Up/Login route --}}">Sign Up/Login</a>
         @endif
     </header>
 
