e230807f251c385a73db7b41cd9a00b1689b1e9a
Kyle Boehlen <kyleboehlen@gmail.com>
2019-11-16 22:22:29 -0700

    About view team section

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,4 +8,15 @@
     {{-- why section --}}
 
     {{-- team section --}}
+    <div class="team-section">
+            <h2>Our Team</h2>
+            <div class="figure-container">
+                @foreach(config('team.members') as $team_member)
+                    <figure>
+                        <img src="{{ route('assets.team', $team_member['name']) }}"/>
+                        <figcaption>{{ $team_member['full_name'] }}</figcaption>
+                    </figure>
+                @endforeach
+            </div>
+        </div>
 @endsection
