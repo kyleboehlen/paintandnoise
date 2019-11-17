fcb18a68c450aa1e8565a635a530ca02441c313f
Kyle Boehlen <kyleboehlen@gmail.com>
2019-11-17 00:42:43 -0700

    Configurable about attribution

--- config/media.php
+++ config/media.php
@@ -2,4 +2,5 @@
 return [
     'path' => storage_path() . env('MEDIA_STORAGE_PATH', '/app/public/media/'),
     'about_image' => env('ABOUT_IMAGE', 'about.jpg'),
+    'about_attribution' => env('ABOUT_ATTRIBUTION', 'unsplash'),
 ];
