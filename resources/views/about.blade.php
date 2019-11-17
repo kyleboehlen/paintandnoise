cabeab126a0f9088cd0a78b5245791b0f1a7cc56
Kyle Boehlen <kyleboehlen@gmail.com>
2019-11-16 23:15:17 -0700

    HTML format on team section

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -9,14 +9,14 @@
 
     {{-- team section --}}
     <div class="team-section">
-            <h2>Our Team</h2>
-            <div class="figure-container">
-                @foreach(config('team.members') as $team_member)
-                    <figure>
-                        <img src="{{ route('assets.team', $team_member['name']) }}"/>
-                        <figcaption>{{ $team_member['full_name'] }}</figcaption>
-                    </figure>
-                @endforeach
-            </div>
+        <h2>Our Team</h2>
+        <div class="figure-container">
+            @foreach(config('team.members') as $team_member)
+                <figure>
+                    <img src="{{ route('assets.team', $team_member['name']) }}"/>
+                    <figcaption>{{ $team_member['full_name'] }}</figcaption>
+                </figure>
+            @endforeach
         </div>
+    </div>
 @endsection
