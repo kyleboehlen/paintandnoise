4659f4086a32c919b71644b77482a43f6806a21d
Kyle Boehlen <kyleboehlen@gmail.com>
2019-12-21 23:14:15 -0700

    Change throttle limit back

--- app/Http/Controllers/Auth/VerificationController.php
+++ app/Http/Controllers/Auth/VerificationController.php
@@ -36,6 +36,6 @@ class VerificationController extends Controller
     {
         $this->middleware('auth');
         $this->middleware('signed')->only('verify');
-        $this->middleware('throttle:1,5')->only('verify', 'resend');
+        $this->middleware('throttle:5,1')->only('verify', 'resend');
     }
 }
