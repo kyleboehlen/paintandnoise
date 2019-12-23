779b7983ad79135a1a015e4dbdfa62696099e282
Kyle Boehlen <kyleboehlen@gmail.com>
2019-12-22 17:51:37 -0700

    Instagram username config typo

--- config/social.php
+++ config/social.php
@@ -3,7 +3,7 @@ return [
     'facebook_url' => env('FACEBOOK_URL', 'https://www.facebook.com/'),
     'facebook_username' => env('FACEBOOK_USERNAME', 'paintandnoise'),
     'instagram_url' => env('INSTAGRAM_URL', 'https://www.instagram.com/'),
-    'instagram_username' => env('INSTAGRAM_URL', 'paintandnoise'),
+    'instagram_username' => env('INSTAGRAM_USERNAME', 'paintandnoise'),
     'twitter_url' => env('TWITTER_URL', 'https://twitter.com/'),
     'twitter_username' => env('TWITTER_USERNAME', 'paintandnoise'),
 ];
