ee317f4960119f1d2374220c3a1b843b62649919
Kyle Boehlen <kyleboehlen@gmail.com>
2020-02-01 13:44:41 -0700

    Posters model and migration

---
+++ app/Models/Posters/Posters.php
@@ -0,0 +1,10 @@
+<?php
+
+namespace App\Models\Posters;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Posters extends Model
+{
+    //
+}
---
+++ database/migrations/2020_02_01_200020_create_posters_table.php
@@ -0,0 +1,45 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreatePostersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('posters', function (Blueprint $table) {
+            // PK
+            $table->uuid('id')->primary(); // UID to use without exposing user_ids
+
+            // Laravel Columns
+            $table->timestamps();
+            $table->softDeletes();
+
+            // Other Columns
+            $table->bigInteger('users_id')->unsigned()->unique();
+            $table->string('slug', 8)->nullable($value = true)->unique();
+            $table->tinyInteger('status_id')->default($value = 3)->unsigned(); // Default value = 'Applied'
+            $table->string('bio', 255)->nullable($value = true);
+
+            // Constraints
+            $table->foreign('users_id')->references('id')->on('users');
+            $table->foreign('status_id')->references('id')->on('posters_statuses');
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('posters');
+    }
+}
