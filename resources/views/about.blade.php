447040e10883bf5f0e7bfaefc83dbf6ef8245d2d
Kyle Boehlen <kyleboehlen@gmail.com>
2020-03-22 15:22:44 -0600

    Fixed typos in about blade template

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -8,7 +8,7 @@
             <img src="{{ route('assets.about') }}" title="Photo by {{ config('media.about_attribution') }}"/>
             <p>
                 Paint and Noise is a platform where you can discover artists, share your art, and affect the creative world.<br/><br/>
-                By being a community of like minded people and inspired creaters we'd like to spark a collaboration and provide a foundation for artists to present their work to an audience as a whole.
+                By being a community of like minded people and inspired creators we'd like to spark a collaboration and provide a foundation for artists to present their work to an audience as a whole.
             </p>
         </div>
     </div>
@@ -18,7 +18,7 @@
         <h2>How Does It Work?</h2>
         <div class="container">
             <p>
-                When artists share their work the community, not some magic algorithm, decides what gets voted to the top<br/><br/>
+                When artists share their work the community, not some magic algorithm, decides what gets voted to the top.<br/><br/>
                 Anybody can sign up to browse content, or vote on which posts they like.<br/><br/>
                 Posts only last {{ config('posts.timeout') }} days so content stays fresh.
             </p>
