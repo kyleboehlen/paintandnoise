e43543ee0acb4142b013911ab142defa4da95b03
Kyle Boehlen <kyleboehlen@gmail.com>
2020-04-05 19:24:51 -0600

    Profile picture added to users test factory

--- database/factories/Users.php
+++ database/factories/Users.php
@@ -17,11 +17,14 @@ use Illuminate\Support\Str;
 */
 
 $factory->define(Users::class, function (Faker $faker) {
+    $test_pic = rand(0, 12);
+
     return [
         'name' => $faker->name,
         'email' => $faker->unique()->safeEmail,
         'email_verified_at' => now(),
         'password' => '$2y$10$92IXUNpkjO0rOQ5byMi.Ye4oKoEa3Ro9llC/.og/at2.uheWG/igi', // password
         'remember_token' => Str::random(10),
+        'profile_picture' => "/test/$test_pic.jpg",
     ];
 });
