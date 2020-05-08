6bf71de0800c14e1ceb81ba45335c30421c5bff9
Kyle Boehlen <kyleboehlen@gmail.com>
2020-05-08 17:21:19 -0600

    Add votes test

--- app/Models/Posts/Votes.php
+++ app/Models/Posts/Votes.php
@@ -4,7 +4,21 @@ namespace App\Models\Posts;
 
 use Illuminate\Database\Eloquent\Model;
 
+// Traits
+use App\Models\Traits\HasCompositePrimaryKey;
+
 class Votes extends Model
 {
-    //
+    use HasCompositePrimaryKey;
+
+    public $incrementing = false;
+    public $timestamps = false;
+
+    protected $primaryKey = [
+        'posts_id', 'users_id',
+    ];
+
+    protected $fillable = [
+        'posts_id', 'users_id',
+    ];
 }
--- tests/TestData/PostsTest.php
+++ tests/TestData/PostsTest.php
@@ -7,7 +7,9 @@ use Illuminate\Foundation\Testing\WithFaker;
 use Tests\TestCase;
 
 // Models
+use App\Models\Users;
 use App\Models\Posts\Posts;
+use App\Models\Posts\Votes;
 
 
 class PostsTest extends TestCase
@@ -25,5 +27,39 @@ class PostsTest extends TestCase
 
         // Verify there are more than 50
         $this->assertTrue(count($posts) >= 50);
+    }
+
+    /**
+     * Seed posts votes
+     *
+     * @return void
+     * @test
+     */
+    public function postsVotesTest()
+    {
+        // Create at least one vote per post
+        $posts = Posts::all();
+        $users = Users::all();
+
+        foreach($posts as $post)
+        {
+            for($i = 0; $i < mt_rand(1, intval($users->count() / 10)); $i++)
+            {
+                $user = $users->random();
+                $ids = array(
+                    'posts_id' => $post->id,
+                    'users_id' => $user->id,
+                );
+                $vote = new Votes($ids);
+                try
+                {
+                    $vote->save();
+                }
+                catch(\Exception $e)
+                { /* skipping duplicate relationship */ }
+            }
+        }
+
+        $this->assertTrue(Votes::all()->count() >= count($posts));
     }
 }
