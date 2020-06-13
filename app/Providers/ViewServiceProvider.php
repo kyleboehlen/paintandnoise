a99ba3c324ac8646dd0ef6eaf30a897258425519
Kyle Boehlen <kyleboehlen@gmail.com>
2020-06-13 13:15:11 -0600

    Moved account and about styles out of _app.scss

--- app/Providers/ViewServiceProvider.php
+++ app/Providers/ViewServiceProvider.php
@@ -24,12 +24,23 @@ class ViewServiceProvider extends ServiceProvider
      */
     public function boot()
     {
+        /** ACCOUNT */
+        View::composer(
+            'account.index', 'App\Http\Composers\AccountComposer'
+        );
+
+        /** ADMIN */
         View::composer(
             'admin.home', 'App\Http\Composers\Admin\HomeComposer'
         );
 
         View::composer(
             'admin.users', 'App\Http\Composers\Admin\UsersComposer'
+        );
+
+        /** ABOUT */
+        View::composer(
+            'about', 'App\Http\Composers\AboutComposer'
         );
     }
 }
