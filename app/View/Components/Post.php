75fef4b304aea619f324fdf9389fde7c61377896
= <=>
2020-09-07 17:06:10 -0600

    Component for post blade template

---
+++ app/View/Components/Post.php
@@ -0,0 +1,40 @@
+<?php
+
+namespace App\View\Components;
+
+use Illuminate\View\Component;
+
+// Constants
+use App\Http\Helpers\Constants\Posts\Types;
+
+// Models
+use App\Models\Categories\Categories;
+use App\Models\Posts\Posts;
+
+class Post extends Component
+{
+    public $category_link;
+    public $post;
+    public $types = Types::class;
+
+    /**
+     * Create a new component instance.
+     *
+     * @return void
+     */
+    public function __construct(Posts $post, $link)
+    {
+        $this->post = $post;
+        $this->category_link = (bool) $link;
+    }
+
+    /**
+     * Get the view / contents that represent the component.
+     *
+     * @return \Illuminate\View\View|string
+     */
+    public function render()
+    {
+        return view('components.post');
+    }
+}
