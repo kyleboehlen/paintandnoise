72aa17dbcce303ba5ea68b823def9bc2cfb40e12
= <=>
2020-09-07 17:07:41 -0600

    Zip radius env variable

--- config/local.php
+++ config/local.php
@@ -1,6 +1,7 @@
 <?php
 return [
     'enabled' => env('LOCAL_FEED_ENABLED', false),
+    'radius' => env('LOCAL_ZIP_RADIUS', 100),
     'zip_wise' => [
         'api_key' => env('ZIP_WISE_API_KEY'),
     ],
