ffee58b7c7ebcf2daa1459ea0f04ba18a5544edf
= <=>
2020-10-07 13:25:42 -0600

    Fix typo in zip code not set alert

--- resources/views/local.blade.php
+++ resources/views/local.blade.php
@@ -4,7 +4,7 @@
     <x-main-nav :highlight="$nav_highlight" />
 
     @if(in_array('zip_alert', $show))
-        <p class="set-zip">Please set update your zip code in your <a href="{{ route('account') }}">account</a> in order to browse the local feed.<p>
+        <p class="set-zip">Please set your zip code in the <a href="{{ route('account') }}">account</a> page in order to browse the local feed.<p>
     @elseif(in_array('posts', $show))
         @foreach($posts as $key => $post)
             <x-post :id="$key" :post="$post" :link="$category_link" />
